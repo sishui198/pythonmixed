645ae6631a8f713d20d619a29cb2834a029ecfef
yzxm365 <yzxm365@163.com>
2017-04-17 12:40:06 +0800

    email的发送邮件（带附件和html）
    email的发送邮件（带附件和html）

---
+++ email/email_fujian_html.py
@@ -0,0 +1,60 @@
+import smtplib
+import time
+
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+
+#创建实例，构造MIMEMulipart对象为根容器
+msg=MIMEMultipart('alternative')
+#正文
+mail_boby='hello.this is the mail content'
+#发信邮箱
+mail_from='yzxm365@sina.cn'
+#收信邮箱
+mail_to=['yzxm365@163.com']
+
+#msg=MIMEText(mail_boby)
+#定义标题
+msg['Subject']=u'this is the title'
+#定义发信人
+msg['From']=mail_from
+msg['To']=';'.join(mail_to)
+msg["date"]=time.strftime('%a, %d %b %Y %H:%M:%S %z')
+time.strftime('%a, %d %b %Y %H:%M:%S %z')
+msg["date"]=time.strftime('%a, %d %b %Y %H:%M:%S %z')
+
+#定义正文
+txt=MIMEText(u'中文内容','plain','utf-8')
+msg.attach(txt)
+#图片路径
+picfiles=[r'C:\Users\te3030\Desktop\33\1.png']
+for file in picfiles:
+    f=open(file,'rb')
+    img=MIMEImage(f.read())
+    f.close()
+    msg.attach(img)
+
+text="Hello!\nHow are you?\nHere is the link you wanted:\nhttp://www.python.org"
+html="""
+<html>
+ <head></head>
+ <body>
+   <p>Hello!<br>
+      How are you?<br>
+      Here is the <a href="http://www.python.org">link</a> you wanted.
+   </p>
+ </body>
+</html>
+"""
+part1=MIMEText(text,'plain')
+part2=MIMEText(html,'html')
+msg.attach(part1)
+msg.attach(part2)
+
+smtp=smtplib.SMTP()
+smtp.connect("smtp.sina.cn")
+smtp.login('yzxm365@sina.cn','zm520Y')
+smtp.sendmail(mail_from,mail_to,msg.as_string())
+smtp.quit()
+print ('ok')
